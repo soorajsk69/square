3abcdcf9af6407e628fa1aa55da8759b4d0dd0aa
sooraj <soorajkumars015@gmail.com>
2023-11-16 02:49:57 +0530

    new update

--- src/components/SinglePageview/Single.jsx
+++ src/components/SinglePageview/Single.jsx
@@ -33,21 +33,23 @@ const Single = () => {
       </div>
 
       {/* Contact Form */}
-      <div className='contact-form'>
-        <h2>Contact Us</h2>
-        <form>
-          <label htmlFor='name'>Name:</label>
-          <input type='text' id='name' name='name' required />
-
-          <label htmlFor='email'>Email:</label>
-          <input type='email' id='email' name='email' required />
-
-          <label htmlFor='message'>Message:</label>
-          <textarea id='message' name='message' required></textarea>
-
-          <button type='submit'>Submit</button>
-        </form>
-      </div>
+   <div className='form-back'>
+        <div className='contact-form'>
+          <h2>Contact Us</h2>
+          <form>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' id='name' name='name' required />
+  
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' name='email' required />
+  
+            <label htmlFor='message'>Message:</label>
+            <textarea id='message' name='message' required></textarea>
+  
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+   </div>
     </div>
   );
 };
